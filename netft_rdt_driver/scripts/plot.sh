7e508c220cdae4a468f5f0ba63825418f15e9f4d
dking <dking@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2011-02-17 22:00:22 +0000

    Change plot script to use Wench instead of Twist.
    git-svn-id: https://code.ros.org/svn/wg-ros-pkg/stacks/netft/trunk@49261 7275ad9f-c29b-430a-bdc5-66f4b3af1622

--- netft_rdt_driver/scripts/plot.sh
+++ netft_rdt_driver/scripts/plot.sh
@@ -6,5 +6,5 @@ if [ -z "$TOPIC" ] ; then
 fi
 
 rxplot -b 10 -p 10 \
-    ${TOPIC}/linear/x,${TOPIC}/linear/y,${TOPIC}/linear/z  \
-    ${TOPIC}/angular/x,${TOPIC}/angular/y,${TOPIC}/angular/z
+    ${TOPIC}/force/x,${TOPIC}/force/y,${TOPIC}/force/z  \
+    ${TOPIC}/torque/x,${TOPIC}/torque/y,${TOPIC}/torque/z
