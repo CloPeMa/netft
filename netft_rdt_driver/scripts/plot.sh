841732d6c4b2567544b80f3834cdd98156dd4ed8
Libor Wagner <wagnelib@cmp.felk.cvut.cz>
2013-09-09 13:12:23 +0200

    Fix plot in hydro

--- netft_rdt_driver/scripts/plot.sh
+++ netft_rdt_driver/scripts/plot.sh
@@ -5,6 +5,6 @@ if [ -z "$TOPIC" ] ; then
     TOPIC="netft_data/wrench"
 fi
 
-rxplot -b 10 -p 10 \
+rqt_plot -b 10 -p 10 \
     ${TOPIC}/force/x,${TOPIC}/force/y,${TOPIC}/force/z  \
     ${TOPIC}/torque/x,${TOPIC}/torque/y,${TOPIC}/torque/z
